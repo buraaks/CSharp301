046ed57c795411a453b98547e63e89c01df6c8b2
buraaks <buraktemur0816@gmail.com>
2025-10-06 13:26:12 +0300

    dataacceslayer entityframework klosörüne sınıflar eklendi

---
+++ CSharp301.DataAccessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,16 @@
+﻿using CSharp301.DataAccessLayer.Abstract;
+using CSharp301.DataAccessLayer.Repositories;
+using CSharp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp301.DataAccessLayer.EntityFramework
+{
+    public class EfCustomerDal:GenericRepository<Customer>, ICustomerDal
+    {
+
+    }
+}
---
+++ CSharp301.DataAccessLayer/EntityFramework/EfOrderDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharp301.DataAccessLayer.Abstract;
+using CSharp301.DataAccessLayer.Repositories;
+using CSharp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp301.DataAccessLayer.EntityFramework
+{
+    public class EfOrderDal:GenericRepository<Order>, IOrderDal
+    {
+    }
+}
